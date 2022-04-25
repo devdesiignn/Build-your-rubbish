83afe966a889172dd384111bfb041131f994ef55
d3vd3511gn <devdesiign@gmail.com>
2022-04-25 14:14:21 +0100

    Remove the finished task code

--- 2-todo-app/script.js
+++ 2-todo-app/script.js
@@ -1,41 +1,33 @@
 // on click the 'add task btn'
 const form = document.querySelector("form");
-const tasksContainer = document.querySelector('#tasks-holder');
+const tasksContainer = document.querySelector("#tasks-holder");
 const addTaskBtn = document.querySelector("#add-task-button");
 const taskInput = document.querySelector("#task-input-field");
 
-tasksContainer.addEventListener('click', function(event) {
+tasksContainer.addEventListener("click", function (event) {
   console.log(event);
-  // ignore if the clicked element is not the delete button 
+  // ignore if the clicked element is not the delete button
   // as we're only interested in handling the delete operation
-  if (!event.target.classList.contains('delete-task-button')) return;
+  if (!event.target.classList.contains("delete-task-button")) return;
 
-  // remote the parent element of that button 
-  event.target.closest('.task-holder').remove()
+  // remove the parent element of that button
+  event.target.closest(".task-holder").remove();
 });
 
 // always use the addEventListener Element class method
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault(); //to prevent the page to auto reload on submit
   if (taskInput.value == "") {
     alert("Please enter a task!");
   } else {
     document.querySelector("#tasks-holder").innerHTML += `
     <div class="task-holder">
-      <span class="task">${
-        taskInput.value
-      }</span>
+      <span class="task">${taskInput.value}</span>
       <button type="submit" class="delete-task-button">Delete</button>
     </div>
     `;
-   
-    //to tag a finished task
-    let finishedTask = document.querySelectorAll(".task");
-    for (counter = 0; counter < finishedTask.length; counter++) {
-      finishedTask[counter].onclick = function () {
-        this.classList.toggle("completed");
-      };
-    }
+
+    
 
     //to clear the input field
     taskInput.value = "";
