1e31f8e5dd8aa5fc875d23d6734634f216052ef7
d3vd3511gn <devdesiign@gmail.com>
2022-04-25 09:59:41 +0100

    Included form onsubmit

--- 2-todo-app/script.js
+++ 2-todo-app/script.js
@@ -1,9 +1,9 @@
 // on click the 'add task btn'
-document.querySelector("#add-task-button").onclick = function () {
-  if (document.querySelector("#task-input-field").value == "") {
-    alert("Please enter a task!");
-  } else {
-    document.querySelector("#tasks-holder").innerHTML += `
+
+document.querySelector("form").onsubmit = function (e) {
+  e.preventDefault(); //to prevent the page to auto reload on submit
+
+  document.querySelector("#tasks-holder").innerHTML += `
     <div class="task-holder">
       <span class="task">${
         document.querySelector("#task-input-field").value
@@ -12,24 +12,25 @@ document.querySelector("#add-task-button").onclick = function () {
     </div>
     `;
 
-    //to delete a task
+  //to delete a task
 
-    let deleteTask = document.querySelectorAll(".delete-task-button");
-    for (counter = 0; counter < deleteTask.length; counter++) {
-      deleteTask[counter].onclick = function () {
-        this.parentNode.remove();
-      };
-    }
-
-    //to tag a finished task
-    let finishedTask = document.querySelectorAll(".task");
-    for (counter = 0; counter < finishedTask.length; counter++) {
-      finishedTask[counter].onclick = function () {
-        this.classList.toggle("completed");
-      };
-    }
-
-    //to clear the input field
-    document.querySelector("#task-input-field").value = "";
+  let deleteTask = document.querySelectorAll(".delete-task-button");
+  for (counter = 0; counter < deleteTask.length; counter++) {
+    deleteTask[counter].onclick = function () {
+      this.parentNode.remove();
+    };
   }
+
+  //to tag a finished task
+
+  let finishedTask = document.querySelectorAll(".task");
+  for (counter = 0; counter < finishedTask.length; counter++) {
+    finishedTask[counter].onclick = function () {
+      this.classList.toggle("completed");
+    };
+  }
+
+  //to clear the input field
+
+  document.querySelector("#task-input-field").value = "";
 };
