6c642af0ff6cf41619609e3bf1cd1d74902edcf5
d3vd3511gn <devdesiign@gmail.com>
2022-06-02 11:54:03 +0100

    Added some interactions

--- nodeJS-code/guess-d-number.js
+++ nodeJS-code/guess-d-number.js
@@ -1,17 +1,35 @@
-const prompt = require("prompt-sync")();
-const number = 12;
-let input;
-const failed = "Wrong!, Try Again!";
-const passed = "Welldone, You passed!";
+const prompt = require("prompt-sync")(); //Calling the prompt-sync module
 
-console.log("This is a Guessing Game".toUpperCase());
+//Variable Declarations
+const number = 7;
+let input;
+const welcomeMsg = "This is a Guessing Game. \n";
+const hintMsg =
+  "HINT: The number you're guessing is less than 0 and greater then 10 \n";
+let noOfTries = 3;
+const passed = "Welldone, You passed! \n";
+const failed = "Wrong!, Try Again! \n";
+
+//Prelimary Messages
+console.log(welcomeMsg.toUpperCase());
+console.log(hintMsg);
+console.log(`You have ${noOfTries} tries`);
+
+//The Guess Function
 function guess() {
-  input = prompt("Enter Number: ");
-  if (input != number) {
-    console.log(failed);
-    guess();
-  } else {
-    console.log(passed.toUpperCase());
+  while (noOfTries > 0) {
+    noOfTries--;
+    input = prompt("Enter Number: ");
+    if (number == input) {
+      console.log(passed.toUpperCase());
+      break;
+    } else {
+      console.log(failed);
+      console.log(`You have ${noOfTries} tries left!`);
+      if (noOfTries == 0) {
+        console.log("Game Over!!!".toUpperCase());
+      }
+    }
   }
 }
 guess();
