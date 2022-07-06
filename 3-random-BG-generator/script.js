8d8c7ed884d3046213b64e4ea199c04338280567
d3vd3511gn <devdesiign@gmail.com>
2022-07-06 03:52:24 +0100

    Refactor && Restructure

--- 3-random-BG-generator/script.js
+++ 3-random-BG-generator/script.js
@@ -1,27 +1,27 @@
 //variable declaration
-let r,
-  g,
-  b,
-  randomColor = ``;
+let randomColor = ``;
 const body = document.querySelector("body");
 const button = document.querySelector("button");
+const tip = document.querySelectorAll(".tip");
 
 //generating a random color
 function generateColor() {
-  r = Math.ceil(Math.random() * 256); //generates a number from 0 to 255
-  g = Math.ceil(Math.random() * 256);
-  b = Math.ceil(Math.random() * 256);
+  const r = Math.ceil(Math.random() * 256); //generates a number from 0 to 255
+  const g = Math.ceil(Math.random() * 256);
+  const b = Math.ceil(Math.random() * 256);
   randomColor = `rgb(${r}, ${g}, ${b})`;
 }
 
 //setting the Background
 function setBg() {
   generateColor(); //generates a random color
-  body.style.background = randomColor; //picks the random color and set it to the body Background
+  body.style.backgroundColor = randomColor; //picks the random color and set it to the body Background
+  tip[0].textContent = `Color: ${randomColor}`;
+  tip[1].textContent = `Color: ${randomColor}`;
 }
 
 //listening for a key press
-body.addEventListener("keydown", function (event) {
+window.addEventListener("keydown", function (event) {
   //checks if the key is SPACE
   if (event.key == " ") {
     setBg(); //then sets the Background
