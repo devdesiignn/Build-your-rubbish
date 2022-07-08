a55b92cbb57618cc5a0784a73d8011c50b7e9282
d3vd3511gn <devdesiign@gmail.com>
2022-07-08 05:23:32 +0100

    commented the test

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -14,4 +14,4 @@
 //   array.reduce((accumulator, currentItem) => accumulator + currentItem, 0);
 
 //test
-console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
