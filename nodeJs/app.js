fdb15d5432a987ccd167d7a5c9a7e7d594a18d00
d3vd3511gn <devdesiign@gmail.com>
2022-07-08 05:18:21 +0100

    Included gitignore

---
+++ nodeJs/app.js
@@ -0,0 +1,17 @@
+//Declare a Function the takes an array as paramenter and
+//Returns the sum of the array
+
+//Named Function Approach
+// const sum = function (array) {
+//   const result = array.reduce(function (accumulator, currentItem) {
+//     return accumulator + currentItem;
+//   }, 0);
+//   return result;
+// };
+
+//Arrow Function Approach
+// const sum = (array) =>
+//   array.reduce((accumulator, currentItem) => accumulator + currentItem, 0);
+
+//test
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
